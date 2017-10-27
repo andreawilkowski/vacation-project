b4176c901212d4b7e1fa68b233e7b0858c4a179d
Andrea Wilkowski <andreawilkowski@yahoo.com>
2017-10-27 09:56:38 -0700

    Add js content, currently trying to debug

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,30 @@
-//backend logic
-
-
-//userinterface logic
 
 $(document).ready(function() {
-  $("").submit(function(event) {
+  $("#questions form").submit(function(event) {
+    var view = parseInt($("select#view").val());
+    var poison = parseInt($("select#poison").val());
+    var date = parseInt($("select#date").val());
+    var admire = parseInt($("select#admire").val());
+    var transportation = parseInt($("select#transportation").val());
 
-  
+    var answer = (view + poison + date + admire + transportation);
+
+
+    if (answer <= 50) {
+      $('#Caribbean').hide();
+      $('#Penthouse').hide();
+      $('#Tree-House').show();
+    } else if (answer >=51 && answer <= 100) {
+      $('#Penthouse').hide();
+      $('#Tree-House').hide();
+      $('#Caribbean').show();
+
+    } else if (answer >=101 && answer <= 150){
+      $('#Caribbean').hide();
+      $('#Tree-House').hide();
+      $('#Penthouse').show();
+    }
+
 
     event.preventDefault();
   });
